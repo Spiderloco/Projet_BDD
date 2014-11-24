7b6d909ce27c1f987e71e7f965db206a7bb75764
VincentDR <vincent.de-rousiers@etud.univ-montp2.fr>
2014-11-24 17:35:45 +0100

    Update remplissage.sql

--- BDD/remplissage.sql
+++ BDD/remplissage.sql
@@ -1,2 +1,6 @@
-insert into DESCRIPTEUR values (Descripteur_T('Alcool'));
-	insert into DESCRIPTEURVEDETTE values (DescripteurVedette_T(Descripteur_T('Alcool')));
+insert into DESCRIPTEUR values (Descripteur_T('Rouge'));
+
+insert into DESCRIPTEURVEDETTE values (DescripteurVedette_T(Descripteur_T('Vin')));
+
+insert into CONCEPT values (Concept_T('Alcool', DescripteurVedette_T(Descripteur_T('Alcool'))));
+
