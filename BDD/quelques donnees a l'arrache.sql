fbf44bc97fb72d4da1439f0b3742442b87918a25
Spiderloco <loic.mercier08@gmail.com>
2014-11-24 17:38:23 +0100

    Create quelques donnees a l'arrache.sql

---
+++ BDD/quelques donnees a l'arrache.sql
@@ -0,0 +1,110 @@
+;; Liste des spiritueux
+INSERT INTO Descripteur (Libelle)
+ VALUES
+    ('Anisette');
+    ('Amers'),
+    ('Amaretto'),
+    ('Aquavit'),
+    ('Absinthe'),
+    ('Arak'),
+    ('Armagnac'),
+    ('Bénédictine'),
+    ('Brandy'),
+    ('Calvados'),
+    ('Chartreuse'),
+    ('Cognac'),
+    ('Curaçao'),
+    ('Floc de Gascogne'),
+    ('Gin'),
+    ('Grappa'),
+    ('Kirsch'),
+    ('Liqueurs'),
+    ('Mei kwei lu chew'),
+    ('Ouzo'),
+    ('Pastis'),
+    ('Pineau des Charentes'),
+    ('Pontarlier'),
+    ('Rakı'),
+    ('Rhum'),
+    ('Sambuca'),
+    ('Tequila'),
+    ('Sodabi'),
+    ('Vins cuits'),
+    ('Vodka'),
+    ('Whisky');
+
+INSERT INTO Descripteur (Libelle)
+ VALUES
+    ('Vin Rouge'),
+    ('Vin Blanc'),
+    ('Vin Rose'),
+    ('Vin Speciales');
+
+INSERT INTO SpecialisationC (ConceptSpecialise, ConceptSpecialisant)
+ VALUES
+    ('Spiritueux', 'Anisette'),
+    ('Spiritueux', 'Amers'),
+    ('Spiritueux', 'Amaretto'),
+    ('Spiritueux', 'Aquavit'),
+    ('Spiritueux', 'Absinthe'),
+    ('Spiritueux', 'Arak'),
+    ('Spiritueux', 'Armagnac'),
+    ('Spiritueux', 'Bénédictine'),
+    ('Spiritueux', 'Brandy'),
+    ('Spiritueux', 'Calvados'),
+    ('Spiritueux', 'Chartreuse'),
+    ('Spiritueux', 'Cognac'),
+    ('Spiritueux', 'Curaçao'),
+    ('Spiritueux', 'Floc de Gascogne'),
+    ('Spiritueux', 'Gin'),
+    ('Spiritueux', 'Grappa'),
+    ('Spiritueux', 'Kirsch'),
+    ('Spiritueux', 'Liqueurs'),
+    ('Spiritueux', 'Mei kwei lu chew'),
+    ('Spiritueux', 'Ouzo'),
+    ('Spiritueux', 'Pastis'),
+    ('Spiritueux', 'Pineau des Charentes'),
+    ('Spiritueux', 'Pontarlier'),
+    ('Spiritueux', 'Rakı'),
+    ('Spiritueux', 'Rhum'),
+    ('Spiritueux', 'Sambuca'),
+    ('Spiritueux', 'Tequila'),
+    ('Spiritueux', 'Sodabi'),
+    ('Spiritueux', 'Vins cuits'),
+    ('Spiritueux', 'Vodka'),
+    ('Spiritueux', 'Whisky');
+
+INSERT INTO SpecialisationC (ConceptSpecialise, ConceptSpecialisant)
+ VALUES
+    ('Vin', 'Vin Rouge'),
+    ('Vin', 'Vin Blanc'),
+    ('Vin', 'Vin Rose'),
+    ('Vin', 'Vin Speciales');
+
+;; liste des Amers
+INSERT INTO Descripteur (Libelle)
+ VALUES
+    ('Abbot\'s Aged Bitters (Baltimore États-Unis)'),
+    ('Amaro Lucano (Italie)'),
+    ('Angostura (Venezuela)'),
+    ('Campari et Fernet Branca (Italie)'),
+    ('China Martini, China Stemmi (Italie, à base de cinchona)'),
+    ('Cynar (Italie, à base d\'artichaut)'),
+    ('Gran Classico (Suisse)'),
+    ('Jägermeister (Allemagne)'),
+    ('Orange bitters (Angleterre)'),
+    ('Picon (est et nord de la France)'),
+    ('Sommer (Alsace)'),
+    ('Suze (apéritif de gentiane français) et ses homologues : Avèze, Salers...'),
+    ('Unicum (Hongrie)'),
+    ('Buff (Luxembourg)'),
+    ('Gancia Americano (Italie)');
+
+;; Liste des Gin
+INSERT INTO Descripteur (Libelle)
+ VALUES
+('Genever (Gin hollandais)'),
+('London Dry Gin (Gin anglais)');
+
+
+
