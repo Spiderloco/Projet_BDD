aa1e709704fe48efd3f2a44f3312d308ba5682bb
vderousiers <vderousiers@i23.etu.info-ufr.univ-montp2.fr>
2014-11-24 15:39:40 +0100

    Creation

--- BDD/creation.sql
+++ BDD/creation.sql
@@ -1,8 +1,10 @@
 drop table SYNONYME;
 drop table SPECIALISATIOND;
 drop table ASSOCIATION;
-drop table DESIGNATION;
 drop table SPECIALISATIONC;
+drop table CONCEPT;
+drop table DESCRIPEURVEDETTE;
+drop table DESCRIPEUR;
 
 drop type Concept_T;
 drop type DescripteurVedette_T;
@@ -14,15 +16,23 @@ create type Descripteur_T as object
 	(Libelle varchar2(25));
 /
 
-create type DescripteurVedette_T from Descripteur_T as object;
+create type DescripteurVedette_T as object
+	(Descripteur Descripteur_T);
 /
 
 create type Concept_T as object
 	(
 		Libelle varchar2(25),
-		Descripteur DescripteurVedette_T
+		DescripteurV DescripteurVedette_T
 	);
 /
+
+create table DESCRIPEUR of Descripteur_T;
+
+create table DESCRIPEURVEDETTE of DescripteurVedette_T;
+
+create table CONCEPT of Concept_T;
+
 
 create table SYNONYME(
 	Descripteur Descripteur_T,
