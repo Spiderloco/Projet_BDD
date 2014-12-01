7d4717e1764e6013abb56cb0bacda7ca0f177082
Spiderloco <loic.mercier08@gmail.com>
2014-12-01 17:37:50 +0100

    Update remplissage.sql

--- BDD/remplissage.sql
+++ BDD/remplissage.sql
@@ -1,6 +1,20 @@
-insert into DESCRIPTEUR values (Descripteur_T('Rouge'));
+--insert into DESCRIPTEUR values (Descripteur_T('Rouge'));
 
-insert into DESCRIPTEURVEDETTE values (DescripteurVedette_T(Descripteur_T('Vin')));
+INSERT INTO DESCRIPTEUR VALUES (
+   Descripteur_T ('Rouge'));
 
-insert into CONCEPT values (Concept_T('Alcool', DescripteurVedette_T(Descripteur_T('Alcool'))));
+--insert into DESCRIPTEURVEDETTE values (DescripteurVedette_T(Descripteur_T('Vin')));
+
+insert into DESCRIPTEUR values (Descripteur_T('Vin'));
+
+INSERT INTO DESCRIPTEURVEDETTE
+if(SELECT DESCRIPTEURVEDETTE_T (REF(d))
+    FROM DESCRIPTEUR d
+    WHERE d.libelle = 'Vin')
+ 
+  SELECT DESCRIPTEURVEDETTE_T (REF(d))
+    FROM DESCRIPTEUR d
+    WHERE d.libelle = 'Vin';
+
+--insert into CONCEPT values (Concept_T('Alcool', DescripteurVedette_T(Descripteur_T('Alcool'))));
 
