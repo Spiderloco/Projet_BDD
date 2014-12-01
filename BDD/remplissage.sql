43fa7deac85e6c354bdc9692193f326f5879b5b3
Spiderloco <loic.mercier08@gmail.com>
2014-12-01 18:11:24 +0100

    Update remplissage.sql

--- BDD/remplissage.sql
+++ BDD/remplissage.sql
@@ -3,18 +3,22 @@
 INSERT INTO DESCRIPTEUR VALUES (
    Descripteur_T ('Rouge'));
 
---insert into DESCRIPTEURVEDETTE values (DescripteurVedette_T(Descripteur_T('Vin')));
 
-insert into DESCRIPTEUR values (Descripteur_T('Vin'));
-
-INSERT INTO DESCRIPTEURVEDETTE
-if(SELECT DESCRIPTEURVEDETTE_T (REF(d))
-    FROM DESCRIPTEUR d
-    WHERE d.libelle = 'Vin')
- 
-  SELECT DESCRIPTEURVEDETTE_T (REF(d))
-    FROM DESCRIPTEUR d
-    WHERE d.libelle = 'Vin';
+--insert into Concept values (Concept_T('Alcool'));
 
 --insert into CONCEPT values (Concept_T('Alcool', DescripteurVedette_T(Descripteur_T('Alcool'))));
+
+insert into DESCRIPTEURVEDETTE values (DescripteurVedette_T(Descripteur_T('Vin')), Concept_T('Alcool'));
+
+--insert into DESCRIPTEUR values (Descripteur_T('Vin'));
+
+--INSERT INTO DESCRIPTEURVEDETTE
+--if(SELECT DESCRIPTEURVEDETTE_T (REF(d))
+ --   FROM DESCRIPTEUR d
+  --  WHERE d.libelle = 'Vin')
+ 
+  --SELECT DESCRIPTEURVEDETTE_T (REF(d))
+   -- FROM DESCRIPTEUR d
+   -- WHERE d.libelle = 'Vin';
+
 
