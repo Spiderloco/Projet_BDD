60dfa948d35800e086e5b3226d26ba0413a89d9c
VincentDR <vincent.de-rousiers@etud.univ-montp2.fr>
2014-12-08 16:29:51 +0100

    Update remplissage.sql

--- BDD/remplissage.sql
+++ BDD/remplissage.sql
@@ -1,24 +1,41 @@
---insert into DESCRIPTEUR values (Descripteur_T('Rouge'));
+/*DESCRIPTEUR*/
+INSERT ALL 
+	INTO DESCRIPTEUR VALUES (Descripteur_T ('Rouge','C est pas blanc'))
+	INTO DESCRIPTEUR VALUES (Descripteur_T ('Blanc','C est pas rouge'))	
+	INTO DESCRIPTEUR VALUES (Descripteur_T ('Red', 'It red'))	
+SELECT * FROM dual;
 
-INSERT INTO DESCRIPTEUR VALUES (
-   Descripteur_T ('Rouge'));
-
-
---insert into Concept values (Concept_T('Alcool'));
-
---insert into CONCEPT values (Concept_T('Alcool', DescripteurVedette_T(Descripteur_T('Alcool'))));
-
-insert into DESCRIPTEURVEDETTE values (DescripteurVedette_T(Descripteur_T('Vin')), Concept_T('Alcool'));
-
---insert into DESCRIPTEUR values (Descripteur_T('Vin'));
-
---INSERT INTO DESCRIPTEURVEDETTE
---if(SELECT DESCRIPTEURVEDETTE_T (REF(d))
- --   FROM DESCRIPTEUR d
-  --  WHERE d.libelle = 'Vin')
- 
-  --SELECT DESCRIPTEURVEDETTE_T (REF(d))
-   -- FROM DESCRIPTEUR d
-   -- WHERE d.libelle = 'Vin';
+/*DESCRIPTEUR VEDETTE*/
+INSERT ALL 
+	INTO DESCRIPTEUR_VEDETTE VALUES (DescripteurVedette_T ('Vin', 'C est pas de la piquette'))
+SELECT * FROM dual;
 
 
+/*CONCEPT*/
+INSERT ALL 
+	INTO CONCEPT VALUES (Concept_t ('Alcool','C est bon!'))
+	INTO CONCEPT VALUES (Concept_t ('Binouze','Pas '))
+SELECT * FROM dual;
+
+
+/*SYNONYME Descripteur-Descripteur*/
+INSERT ALL 
+	INTO SYNONYME  VALUES ('Rouge','Red')
+SELECT * FROM dual;
+
+
+/*ASSOCIATION Descripteur-DescripteurVedette*/
+INSERT ALL 
+	INTO ASSOCIATION VALUES	('Rouge', 'Vin')
+SELECT * FROM dual;
+
+
+/*SPECIALISATION_DESCRIPTEUR DescripteurVedette-Concept*/
+INSERT ALL 
+	INTO SPECIALISATION_DESCRIPTEUR VALUES	('Vin', 'Alcool')
+SELECT * FROM dual;	
+
+/*SPECIALISATION_CONCEPT Concept-Concept qui est spécialisé*/
+INSERT ALL 
+	INTO SPECIALISATION_CONCEPT VALUES	('Binouze','Alcool')
+SELECT * FROM dual;
