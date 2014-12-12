b253ac6703cd4c91f081481aeb35647985f9f8bf
Spiderloco <loic.mercier08@gmail.com>
2014-12-12 17:19:21 +0100

    Create spiritueux.sql

---
+++ BDD/spiritueux.sql
@@ -0,0 +1,274 @@
+;; Liste des spiritueux
+
+INSERT ALL
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Aquavit', 'L''aquavit est une eau-de-vie de céréale ou de pomme de terre parfumée avec différentes substances aromatiques (cumin, anis, fenouil, cannelle, orange amère, etc.).')
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Arak', 'L''Arak est préparé à partir de jus de raisin distillé en eau-de-vie auquel on ajoute des graines d''anis.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Armagnac', 'L''armagnac est une eau-de-vie de vin produite dans les départements français du Gers, des Landes et du Lot-et-Garonne'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Brandy', 'Brandy est un terme de langue anglaise désignant une eau-de-vie. Il vient du néerlandais brandewijn signifiant « vin brûlé ». Seul, ce terme s''applique plutôt à une eau-de-vie de raisin (cognac, armagnac, rakia, etc.). Associé à un nom de fruit il désigne un spiritueux provenant de ce fruit, par exemple apricot brandy, cherry brandy.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Calvados', 'Le calvados (appelé familièrement « calva ») est une eau-de-vie d''origine normande obtenue par distillation de cidre ou de poiré.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Cognac', 'Le cognac est une eau-de-vie fine à base de raisin, produite dans une région délimitée centrée autour de Cognac. Elle doit respecter des normes et des règles de production bien précises afin de pouvoir obtenir l''appellation « cognac ».'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Floc de Gascogne', 'Le floc de Gascogne est un vin de liqueur, ou mistelle, élaboré par mélange de « moût » de raisin et d''armagnac jeune.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Gin', 'Le gin est une boisson obtenue en aromatisant de l''alcool rectifié d''origine agricole avec des baies de genévrier.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Grappa', 'La grappa est une eau-de-vie de marc de raisin produite en Italie, dans le canton suisse du Tessin et à Saint-Marin.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Kirsch', 'Le kirsch est une eau-de-vie obtenue par double distillation de jus fermentés de cerises'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Liqueurs', 'Une liqueur est une boisson spiritueuse ayant une teneur en sucre minimale de 100 grammes par litre, obtenue par aromatisation de l''alcool éthylique d''origine agricole ou d''un distillat d''origine agricole.'))
+
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Mei kwei lu chew', 'Cet alcool est généralement servi en fin de repas dans les restaurants chinois.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Ouzo', 'L''ouzo est une boisson alcoolisée anisée grecque fabriquée à base d''alcool neutre mélangé à divers aromates (principalement l''anis) puis distillé et dilué.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Pastis', 'Le pastis (du provençal pastis : « mélange ») est le nom donné à des boissons alcoolisées parfumées à l''anis et à la réglisse.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Pineau des Charentes', 'Le pineau des Charentes est une mistelle, une boisson alcoolisée obtenue par mélange de moût et d''eau-de-vie de cognac.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Pontarlier', 'Le pontarlier est une dénomination informelle désignant un apéritif alcoolisé anisé confectionné dans la région de Pontarlier en Franche-Comté'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Rakı', 'Le raki est une eau-de-vie consommée en Serbie, en Albanie, en Turquie, au Kosovo, en Arménie et plus généralement dans le Moyen-Orient, aromatisée à l’anis, principalement à base de raisin.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Rhum' , 'Le rhum est une eau-de-vie originaire des Amériques, produite à partir de la canne à sucre ou de sous-produits de l''industrie sucrière. Il est consommé blanc, vieilli en fût (rhum vieux) ou épicé. Il prend alors une coloration ambrée plus ou moins foncée. En fonction de la matière première utilisée, il peut être appelé agricole ou industriel.'))
+
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Tequila', 'Le ou la tequila, est une boisson alcoolisée produite au Mexique à partir d''une plante nommée Agave tequilana.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Sodabi'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Vins cuits'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Vodka', 'La vodka est une boisson alcoolisée incolore de 40 degrés. L''origine de cette eau-de-vie se situerait en Russie ou en Pologne selon les sources. Il s''agit généralement d''une eau-de-vie de pomme de terre ou de céréales, mais d''autres matières premières peuvent être utilisées.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Whisky', 'Le whisky (ou whiskey) est le nom générique d''un ensemble d''eaux-de-vie fabriquées par distillation de céréales maltées ou non maltées.'))
+SELECT * FROM dual;
+
+
+-- liste des Liqueurs
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Absinthe', 'L''absinthe est un ensemble de spiritueux à base de plantes d''absinthe, également appelé « fée verte » ou encore « bleue ».'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Amaretto', 'L''amaretto est l''une des liqueurs pouvant servir à aromatiser le tiramisù, un dessert italien. Une boisson qui peut s''apprécier autant à l''apéritif (sur glace ou pour beaucoup de cocktails) qu''au digestif, ainsi qu''en shot dans un cadre festif.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Amaro', 'Amaro (en français: amer) est un terme italien qui désigne une variété de liqueur à base de plantes.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Anisette', 'une liqueur à base d''anis vert et de diverses autres plantes, dont le degré d''alcool varie de 20 à 25 ° d''alcool.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Bénédictine', 'La Bénédictine est une boisson alcoolisée digestive fabriquée à Fécamp en Normandie.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Amer', 'L''amer (appelé aussi amère ou bitter) est une liqueur apéritive fabriquée à partir de l''infusion de plantes amères. Certains amers entrent dans la composition de différents cocktails.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Chartreuse', 'La chartreuse est une liqueur fabriquée par les moines chartreux dans les caves de Voiron en Isère, en bordure du Massif de la Chartreuse.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Cointreau', 'Le cointreau est une liqueur à base d''écorces d''oranges douces et amères (Citrus bigaradia), créée en 1875 à Angers, proche du triple sec. C''est aussi une marque appartenant à l''entreprise Rémy Cointreau.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Curaçao', 'Le curaçao est une liqueur d''orange titrant entre 20 ° et 40 ° utilisée pour confectionner de nombreux cocktails.'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Drambuie'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Génépi', 'Ce nom désigne la liqueur obtenue par la macération des plantes Artemisia.'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Liqueur de genièvre'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Liqueur de gentiane'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Gold Strike'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Grand Marnier'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Guignolet'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Izarra'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Jägermeister'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Kahlua'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Kummel'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Licor 43'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Liqueur de myrte'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Millz Premium'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Noyau de Poissy'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Liqueur de sapin'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Persico'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Liqueur de Poejo'))
+--INTO DESCRIPTEUR VALUES (Descripteur_T ('Rhubarbe Zucca'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Sambuca', 'La sambuca est le nom donné à une liqueur italienne douce et forte à base d''anis étoilé, créée à Civitavecchia, dans la province de Rome, en 1851.'))
+--INTO Descripteur VALUES (Descripteur_T ('Suze'))
+--INTO Descripteur VALUES (Descripteur_T ('Thibarine'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Triple sec', 'Un triple sec est une liqueur d''écorces d''orange utilisée dans de très nombreux cocktails et recettes. Elle résulte d''une triple distillations d''écorces d''oranges douces et amères.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Vermouth', 'Le vermouth est un apéritif à base de vin blanc, obtenu par addition de sucre ou de mistelle (mélange de moût de raisin et d''alcool), d''alcool neutre et aromatisé par une infusion de nombreuses plantes aromatiques. Il titre entre 16° et 18° d''alcool. Le vermouth rouge doit sa couleur à l''adjonction de caramel.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Verveine', 'La liqueur de verveine est une production artisanale, familiale surtout, que l''on trouve dans les Alpes et en Haute-Loire (Verveine du Velay), et probablement ailleurs, fabriquée à partir de la verveine citronnelle.'))
+INTO DESCRIPTEUR VALUES (Descripteur_T ('Vespetrò', 'Le Vespetrò est une liqueur italienne d''origine savoyarde ancienne aux vertus carminatives reconnues'))
+
+-- liste des Amers
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Abbot''s Aged Bitters', '(Baltimore États-Unis)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Amaro Lucano', '(Italie)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Angostura' , '(Venezuela)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Campari', '(Italie)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Fernet Branca', '(Italie)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('China Martini', '(Italie, à base de cinchona)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Cynar','(Italie, à base d''artichaut)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Gran Classico', '(Suisse)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Jägermeister', '(Allemagne)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Orange bitters', '(Angleterre)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Picon', '(est et nord de la France)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Sommer', '(Alsace)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Suze', '(apéritif de gentiane français) et ses homologues : Avèze, Salers...'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Unicum', '(Hongrie)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Buff', '(Luxembourg)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Gancia Americano', '(Italie)'))
+
+-- Liste des Gin
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Genever' , '(Gin hollandais)'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('London Dry Gin', 'Gin anglais'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Old tom Gin', 'Gin légèrement sucré'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Yellow gin', 'gin vieilli en fûts de chêne ayant contenue du Xérès'))
+INTO DESCRIPTEUR VALUES  (Descripteur_T ('Cork Gin', 'gin irlandais'))
+
+SELECT * FROM dual;
+-- FIN DES DESCRIPTEURS
+
+
+
+--DESCRIPTEUR VEDETTE
+INSERT ALL
+--
+INTO DESCRIPTEUR_VEDETTE VALUES ('Alcool')
+--
+INTO DESCRIPTEUR_VEDETTE VALUES ('Spiritueux')
+--
+-- Spiritueux
+INTO DESCRIPTEUR_VEDETTE VALUES ('Anisette')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Amers')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Amaretto')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Aquavit')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Absinthe')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Arak')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Armagnac')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Bénédictine')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Brandy')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Calvados')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Chartreuse')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Cognac')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Curaçao')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Floc de Gascogne')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Gin')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Grappa')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Kirsch')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Liqueurs')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Mei kwei lu chew')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Ouzo')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Pastis')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Pineau des Charentes')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Pontarlier')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Rakı')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Rhum')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Sambuca')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Tequila')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Sodabi')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Vins cuits')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Vodka')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Whisky') 
+--
+-- Liqueurs
+INTO DESCRIPTEUR_VEDETTE VALUES ('Absinthe')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Amers')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Anisette')
+INTO DESCRIPTEUR_VEDETTE VALUES ('Bénédictine')
+SELECT * FROM dual;
+-- FIN DES DESCRIPTEURS VEDETTE
+
+
+--CONCEPT
+INSERT ALL
+INTO CONCEPT VALUES ('Alcool')
+--
+INTO CONCEPT VALUES ('Spiritueux')
+--
+-- Spiritueux
+INTO CONCEPT VALUES ('Amaretto')
+INTO CONCEPT VALUES ('Aquavit')
+INTO CONCEPT VALUES ('Arak')
+INTO CONCEPT VALUES ('Armagnac')
+INTO CONCEPT VALUES ('Brandy')
+INTO CONCEPT VALUES ('Calvados')
+INTO CONCEPT VALUES ('Chartreuse')
+INTO CONCEPT VALUES ('Cognac')
+INTO CONCEPT VALUES ('Curaçao')
+INTO CONCEPT VALUES ('Floc de Gascogne')
+INTO CONCEPT VALUES ('Gin')
+INTO CONCEPT VALUES ('Grappa')
+INTO CONCEPT VALUES ('Kirsch')
+INTO CONCEPT VALUES ('Liqueurs')
+INTO CONCEPT VALUES ('Mei kwei lu chew')
+INTO CONCEPT VALUES ('Ouzo')
+INTO CONCEPT VALUES ('Pastis')
+INTO CONCEPT VALUES ('Pineau des Charentes')
+INTO CONCEPT VALUES ('Pontarlier')
+INTO CONCEPT VALUES ('Rakı')
+INTO CONCEPT VALUES ('Rhum')
+INTO CONCEPT VALUES ('Sambuca')
+INTO CONCEPT VALUES ('Tequila')
+INTO CONCEPT VALUES ('Sodabi')
+INTO CONCEPT VALUES ('Vins cuits')
+INTO CONCEPT VALUES ('Vodka')
+INTO CONCEPT VALUES ('Whisky') 
+
+-- Liqueurs
+INTO CONCEPT VALUES ('Absinthe')
+INTO CONCEPT VALUES ('Amers')
+INTO CONCEPT VALUES ('Anisette')
+INTO CONCEPT VALUES ('Bénédictine')
+
+-- Amers
+
+-- Gin ?
+SELECT * FROM dual;
+-- FIN DES CONCEPTS
+
+-- ASSOCIATIONS Descripteur-DescripteurVedette
+INSERT ALL
+	-- Les Spiritueux
+	INTO ASSOCIATION VALUES ('Amaretto' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Aquavit' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Arak' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Armagnac' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Brandy' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Calvados' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Chartreuse' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Cognac' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Curaçao', 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Floc de Gascogne' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Gin' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Grappa' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Kirsch' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Liqueurs' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Mei kwei lu chew' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Ouzo' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Pastis' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Pineau des Charentes' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Pontarlier' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Rakı' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Rhum' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Sambuca' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Tequila' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Sodabi' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Vins cuits' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Vodka' , 'Spiritueux')
+	INTO ASSOCIATION VALUES ('Whisky' , 'Spiritueux')
+
+	-- Les Liqueurs
+	INTO ASSOCIATION VALUES ('Absinthe' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Amaretto' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Amaro' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Anisette' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Amer' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Chartreuse' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Cointreau' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Curaçao' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Génépi' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Triple sec' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Vermouth' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Verveine' , 'Liqueurs' )
+	INTO ASSOCIATION VALUES ('Vespetrò' , 'Liqueurs' )
+
+	-- Les Amers
+	INTO ASSOCIATION VALUES ('Abbot''s Aged Bitters' , 'Amer')
+	INTO ASSOCIATION VALUES ('Amaro Lucano' , 'Amer')
+	INTO ASSOCIATION VALUES ('Angostura' , 'Amer')
+	INTO ASSOCIATION VALUES ('Campari' , 'Amer')
+	INTO ASSOCIATION VALUES ('Fernet Branca' , 'Amer')
+	INTO ASSOCIATION VALUES ('China Martini' , 'Amer')
+	INTO ASSOCIATION VALUES ('Cynar' , 'Amer')
+	INTO ASSOCIATION VALUES ('Gran Classico' , 'Amer')
+	INTO ASSOCIATION VALUES ('Jägermeister' , 'Amer')
+	INTO ASSOCIATION VALUES ('Orange bitters' , 'Amer')
+	INTO ASSOCIATION VALUES ('Picon' , 'Amer')
+	INTO ASSOCIATION VALUES ('Sommer' , 'Amer')
+	INTO ASSOCIATION VALUES ('Suze' , 'Amer')
+	INTO ASSOCIATION VALUES ('Unicum' , 'Amer')
+	INTO ASSOCIATION VALUES ('Buff' , 'Amer')
+	INTO ASSOCIATION VALUES ('Gancia Americano' , 'Amer')
+
+	-- Les Gin
+	INTO ASSOCIATION VALUES ('Genever' , 'Gin')
+	INTO ASSOCIATION VALUES ('London Dry Gin' , 'Gin')
+	INTO ASSOCIATION VALUES ('Old tom Gin' , 'Gin')
+	INTO ASSOCIATION VALUES ('Yellow gin' , 'Gin')
+	INTO ASSOCIATION VALUES ('Cork Gin' , 'Gin')
+SELECT * FROM dual;
+
+--SPECIALISATION_CONCEPT Concept-Concept qui est spécialisé
+INSERT ALL
+-- ex :: INTO SPECIALISATION_CONCEPT VALUES('Bourgogne', 'Vin')
+-- donc : INTO SPECIALISATION_CONCEPT VALUES('Liqueur', 'Spiritueux') ?
+-- donc : INTO SPECIALISATION_CONCEPT VALUES('Vodka', 'Spiritueux') ?
+SELECT * FROM dual;
+
+
