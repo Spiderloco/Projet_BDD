02a5d713429c6e0a405b94821cca1fba04b0d0b0
Jeje34 <pastorjeremy34@gmail.com>
2014-12-18 18:06:14 +0100

    Update lettres.php

--- Interface/lettres.php
+++ Interface/lettres.php
@@ -4,7 +4,7 @@
 	echo '<ul id="listeLettres">';
 	echo '<li><strong>Recherche par lettre :</strong></li>';
 	    for($i=0;$i<=25;$i++){
-		 echo '<li><a href="index.php?page=recherche?lettre='.chr(65+$i).'">'.chr(65+$i).'</a></li>';
+		 echo '<li><a href="index.php?page=recherche&lettre='.chr(65+$i).'">'.chr(65+$i).'</a></li>';
 	    }
 	echo '</ul>';
 ?>
