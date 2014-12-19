9583e6c3c8fadd350869d34c6723644434e56966
Jeje34 <pastorjeremy34@gmail.com>
2014-12-19 13:10:37 +0100

    casse + recherche par mots via lettres
    Plus de sensiblité à la casse (majuscules/mininuscules)
    Lien vers la "page" du mot après une recherche par lettre

--- Interface/recherche.php
+++ Interface/recherche.php
@@ -1,7 +1,7 @@
 
-
-
 <?php 
+
+	// connexion à la base de données
 	$tns = "
 	   (DESCRIPTION =
 	   (ADDRESS_LIST =
@@ -20,48 +20,58 @@
 		echo ($e->getMessage());
 	}
 
+
+	// recherche pas lettre
 	if (isset($_GET['lettre']) && !empty($_GET['lettre'])){
 		$lettreSelectionnee = $_GET['lettre'];
 		$requete = $sth->prepare('SELECT libelle FROM descripteur WHERE libelle LIKE ?');
-		$requete->execute(array("$lettreSelectionnee%"));
-	
-		while ($data = $requete->fetch(PDO::FETCH_ASSOC)) {
-			//echo $data['LIBELLE'] . '</br>';
-			$_POST['termeSaisi'] = $data['LIBELLE'];
-			print_r($_POST);
-			echo '<li><a href="index.php?page=recherche">'.$data['LIBELLE'].'</a></li>';
-		}
-	}
-	else {
-
-		if (isset($_POST['termeSaisi']) && !empty($_POST['termeSaisi'])) { ?>
-			<h1><?php echo $_POST['termeSaisi']; ?></h1>
-
+		$requete->execute(array("$lettreSelectionnee%")); 
+		echo '<h2>Termes en "' . $lettreSelectionnee . '" :</h2>'; ?>
+		<ul id="listeTermes"> 
 		<?php
-			$termeSaisi = $_POST['termeSaisi'];
-			echo $termeSaisi;
-			$requete = $sth->prepare('SELECT description FROM descripteur WHERE libelle = :terme');
-			$requete->bindParam(':terme', $termeSaisi, PDO::PARAM_STR, 30);
-			$requete->execute();
-	
-			if($data = $requete->fetch(PDO::FETCH_ASSOC)) { ?>
-
-				<h2>Definition</h2>
-				<?php   
-					echo $data['DESCRIPTION'] . '<br/>';?>
-				<h2>Synonymes</h2>
-
-				<h2>Est generalise par</h2>
-				<h2>Est specialise par</h2>
-				<h2>Autres associations</h2>
-
-			<?php } 
-			else {
-				echo 'Aucun terme ne correspond!';
-			}
+		while ($data = $requete->fetch(PDO::FETCH_ASSOC)) {
+			echo '<li><a href="index.php?page=recherche&termeSelectionne=' . $data['LIBELLE'] . '">' . $data['LIBELLE'] . '</a></li>';
 		}
-	} ?>
+		print_r($data);
+		?>
+		</ul>
+		<?php
+	}
 
+	// recherche par mots
+	else {
+		// si recherche par mots via le formulaire à l'accueil
+		if (isset($_POST['termeSaisi']) && !empty($_POST['termeSaisi'])) {
+			$terme = strtolower($_POST['termeSaisi']);
+		}
+		// si recherche par mots via la recherche par lettre
+		if (isset($_GET['termeSelectionne']) && !empty($_GET['termeSelectionne'])) {
+			$terme = strtolower($_GET['termeSelectionne']);
+		}
+		?>
+
+		<!-- requetes et affichage -->
+		<h1><?php echo $terme; ?></h1>
+
+		<?php 
+		$requete = $sth->prepare('SELECT description FROM descripteur WHERE LOWER(libelle) = :terme');
+		$requete->bindParam(':terme', $terme, PDO::PARAM_STR, 30);
+		$requete->execute();
+
+		if($data = $requete->fetch(PDO::FETCH_ASSOC)) { ?>
+			<h2>Definition</h2>
+			<?php echo $data['DESCRIPTION'] . '<br/>';?>
+			<h2>Synonymes</h2>
+			<h2>Est generalise par</h2>
+			<h2>Est specialise par</h2>
+			<h2>Autres associations</h2>
+
+		<?php } 
+		else {
+			echo 'Aucun terme ne correspond!';
+		}
+
+	} ?>
 
 
 
