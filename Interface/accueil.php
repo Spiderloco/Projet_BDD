5f4f5a5a2859460d2910ce40772fe47537594bee
Jeje34 <pastorjeremy34@gmail.com>
2015-01-05 10:45:03 +0100

    Update accueil.php

--- Interface/accueil.php
+++ Interface/accueil.php
@@ -1,12 +1,7 @@
-
-
-
-
 <div id="formulaireRecherche">
 <form method="post" action="index.php?page=recherche">
-	<p> <label id="labelTermeSaisi" for="termeSaisi"> Rechercher des termes associés à : </label></br>
+	<p> <label id="labelTermeSaisi" for="termeSaisi"> Rechercher un terme : </label></br>
 	<input id="termeSaisi" name="termeSaisi" type="text" value="" size="40" > </p>
 	<p><input type="submit" name="boutonRechercher" id="boutonRechercher" value="RECHERCHER" ></p>
 </form>
 </div>
-
